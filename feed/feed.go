f19e24a997b4e7d6e785d6100633ebc125e6e4cf
Benjamin Walkenhorst <krylon@posteo.de>
2021-02-01 13:14:01 +0100

    Feed - basics (incomplete)

--- feed/feed.go
+++ feed/feed.go
@@ -2,13 +2,20 @@
 // -*- mode: go; coding: utf-8; -*-
 // Created on 01. 02. 2021 by Benjamin Walkenhorst
 // (c) 2021 Benjamin Walkenhorst
-// Time-stamp: <2021-02-01 10:37:08 krylon>
+// Time-stamp: <2021-02-01 13:09:53 krylon>
 
 // Package feed provides the basic data type and logic to represent and interact
 // with RSS feeds.
 package feed
 
-import "time"
+import (
+	"log"
+	"ticker/common"
+	"ticker/logdomain"
+	"time"
+
+	"github.com/SlyMarbo/rss"
+)
 
 // Feed represents an RSS feed.
 type Feed struct {
@@ -18,7 +25,27 @@ type Feed struct {
 	Interval   time.Duration
 	LastUpdate time.Time
 	Active     bool
+	rfeed      *rss.Feed
+	log        *log.Logger
 }
+
+// New creates a new Feed.
+func New(id int64, name, url string, interval time.Duration, active bool) (*Feed, error) {
+	var err error
+	var f = &Feed{
+		ID:       id,
+		Name:     name,
+		URL:      url,
+		Interval: Interval,
+		Active:   active,
+	}
+
+	if f.log, err = common.GetLogger(logdomain.Feed); err != nil {
+		return nil, err
+	}
+
+	return f, nil
+} // func New(name, url string, interval time.Duration, active bool) (*Feed, error)
 
 // IsDue returns true if the Feed is due for a refresh.
 func (f *Feed) IsDue() bool {
@@ -29,3 +56,24 @@ func (f *Feed) IsDue() bool {
 func (f *Feed) Next() time.Time {
 	return f.LastUpdate.Add(f.Interval)
 } // func (f *Feed) Next() time.Time
+
+// Fetch fetches a Feed.
+func (f *Feed) Fetch() (*rss.Feed, error) {
+	var (
+		err error
+		fd  *rss.Feed
+	)
+
+	if f.rfeed != nil {
+		fd = f.rfeed
+	} else if fd, err = rss.Fetch(f.URL); err != nil {
+		f.log.Printf("[ERROR] Error fetching %s (%s): %s\n",
+			f.Name,
+			f.URL,
+			err.Error())
+		return nil, err
+	}
+
+	f.rfeed = fd
+
+} // func (f *Feed) Fetch() (*rss.Feed, error)
