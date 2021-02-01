cbf3f2766aba56d620db41c23769a1736195fdca
Benjamin Walkenhorst <krylon@posteo.de>
2021-02-01 09:19:17 +0100

    Add package logdomain

---
+++ logdomain/logdomain.go
@@ -0,0 +1,26 @@
+// /home/krylon/go/src/ticker/logdomain/logdomain.go
+// -*- mode: go; coding: utf-8; -*-
+// Created on 01. 02. 2021 by Benjamin Walkenhorst
+// (c) 2021 Benjamin Walkenhorst
+// Time-stamp: <2021-02-01 09:16:14 krylon>
+
+// Package logdomain provides constants to identify the different
+// "areas" of the application that perform logging.
+package logdomain
+
+//go:generate stringer -type=ID
+
+// ID represents an area of concern.
+type ID uint8
+
+// These constants identify the various logging domains.
+const (
+	Common ID = iota
+)
+
+// AllDomains returns a slice of all the known log sources.
+func AllDomains() []ID {
+	return []ID{
+		Common,
+	}
+} // func AllDomains() []ID
