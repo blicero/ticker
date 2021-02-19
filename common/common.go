80ab8bd4656020b038c23d2de172ac77730e93ac
Benjamin Walkenhorst <krylon@posteo.de>
2021-02-19 19:58:58 +0100

    Bump version number

--- common/common.go
+++ common/common.go
@@ -2,7 +2,7 @@
 // -*- mode: go; coding: utf-8; -*-
 // Created on 01. 02. 2021 by Benjamin Walkenhorst
 // (c) 2021 Benjamin Walkenhorst
-// Time-stamp: <2021-02-16 13:43:37 krylon>
+// Time-stamp: <2021-02-19 00:08:34 krylon>
 
 // Package common contain definitions used throughout the application
 package common
@@ -35,7 +35,7 @@ import (
 // application.
 const (
 	AppName                  = "Ticker"
-	Version                  = "0.3.0"
+	Version                  = "0.4.0"
 	Debug                    = true
 	TimestampFormatMinute    = "2006-01-02 15:04"
 	TimestampFormat          = "2006-01-02 15:04:05"
