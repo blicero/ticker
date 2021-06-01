9d4f39c8a4693c625502f2cffd1109836f44866d
Benjamin Walkenhorst <krylon@posteo.de>
2021-06-02 01:03:35 +0200

    Implement Prefetch (still untested), adjust go modules

--- go.mod
+++ go.mod
@@ -9,6 +9,7 @@ require (
 	github.com/blicero/krylib v0.0.0-20210224100727-002c413214d3
 	github.com/dchest/stemmer v0.0.0-20161207102402-66719a20c4b5
 	github.com/endeveit/guesslanguage v0.0.0-20141216121003-b9ec07401dcb
+	github.com/go-shiori/dom v0.0.0-20210510042244-c74b4cff1404
 	github.com/gorilla/mux v1.8.0
 	github.com/hashicorp/logutils v1.0.0
 	github.com/jaytaylor/html2text v0.0.0-20200412013138-3577fbdbcff7
@@ -18,6 +19,5 @@ require (
 	github.com/odeke-em/go-uuid v0.0.0-20151221120446-b211d769a9aa
 	github.com/olekukonko/tablewriter v0.0.5 // indirect
 	github.com/ssor/bom v0.0.0-20170718123548-6386211fdfcf // indirect
-	golang.org/x/net v0.0.0-20210224082022-3d97a244fca7 // indirect
-	golang.org/x/text v0.3.5 // indirect
+	golang.org/x/net v0.0.0-20210505214959-0714010a04ed
 )
--- prefetch/prefetch.go
+++ prefetch/prefetch.go
@@ -2,7 +2,7 @@
 // -*- mode: go; coding: utf-8; -*-
 // Created on 31. 05. 2021 by Benjamin Walkenhorst
 // (c) 2021 Benjamin Walkenhorst
-// Time-stamp: <2021-06-01 01:07:41 krylon>
+// Time-stamp: <2021-06-02 00:27:06 krylon>
 
 // Package prefetch processes items received via RSS/Atom feeds
 // and checks if they contain image links.
@@ -12,13 +12,27 @@
 package prefetch
 
 import (
+	"bytes"
+	"fmt"
+	"io"
 	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"path"
+	"path/filepath"
+	"regexp"
+	"strings"
 	"sync"
 	"ticker/common"
 	"ticker/database"
 	"ticker/feed"
 	"ticker/logdomain"
 	"time"
+
+	"github.com/go-shiori/dom"
+
+	"golang.org/x/net/html"
 )
 
 const (
@@ -158,6 +172,174 @@ func (p *Prefetcher) receiver(db *database.Database) {
 } // func (p *Prefetcher) receiver(db *database.Database)
 
 func (p *Prefetcher) worker() {
+	var ticker = time.NewTicker(delay)
+	defer ticker.Stop()
+
 	for p.IsRunning() {
+		var (
+			err  error
+			body string
+		)
+
+		select {
+		case <-ticker.C:
+			continue
+		case i := <-p.procQ:
+			if body, err = p.sanitize(&i); err != nil {
+				p.log.Printf("[ERROR] Cannot process item %d (%q): %s\n",
+					i.ID,
+					i.Title,
+					err.Error())
+			} else {
+				p.resQ <- processedItem{item: i, body: body}
+			}
+		}
 	}
 } // func (p *Prefetcher) worker()
+
+func (p *Prefetcher) sanitize(i *feed.Item) (string, error) {
+	var (
+		err error
+		rdr *strings.Reader
+		doc *html.Node
+		lnk *url.URL
+	)
+
+	if lnk, err = url.Parse(i.URL); err != nil {
+		p.log.Printf("[ERROR] Cannot parse Item URL %q: %s\n",
+			i.URL,
+			err.Error())
+		return "", err
+	}
+
+	rdr = strings.NewReader(i.Description)
+
+	if doc, err = html.Parse(rdr); err != nil {
+		return "", err
+	}
+
+	for _, node := range dom.GetElementsByTagName(doc, "img") {
+		var (
+			uri                 *url.URL
+			localpath, basename string
+			href                = dom.GetAttribute(node, "src")
+		)
+
+		if uri, err = url.Parse(href); err != nil {
+			p.log.Printf("[ERROR] Cannot process URL %q: %s\n",
+				href,
+				err.Error())
+			continue
+		} else if !uri.IsAbs() {
+			uri = lnk.ResolveReference(uri)
+		}
+
+		if localpath, err = p.fetchImage(uri.String()); err != nil {
+			p.log.Printf("[ERROR] Error fetching image %q: %s\n",
+				uri,
+				err.Error())
+			return "", err
+		}
+
+		basename = path.Base(localpath)
+
+		href = "/cache/" + basename
+
+		dom.SetAttribute(node, "src", href)
+	}
+
+	var buf bytes.Buffer
+
+	if err = html.Render(&buf, doc); err != nil {
+		p.log.Printf("[ERROR] Cannot render DOM tree back to HTML: %s\n",
+			err.Error())
+	}
+
+	return buf.String(), nil
+
+	// return "", krylib.ErrNotImplemented
+} // func (p *Prefetcher) sanitize(i *feed.Item) (string, error)
+
+// Chances are pretty low, I suppose, but we could make a little effort to avoid
+// duplicates.
+// ... Not today, though! (2021-06-01)
+
+func (p *Prefetcher) fetchImage(href string) (string, error) {
+	var (
+		err                      error
+		resp                     *http.Response
+		localPath, cksum, suffix string
+		fh                       *os.File
+	)
+
+	if resp, err = http.Get(href); err != nil {
+		p.log.Printf("[ERROR] Failed to fetch %q: %s\n",
+			href,
+			err.Error())
+		return "", err
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		err = fmt.Errorf("Failed to fetch %q: %s",
+			href,
+			resp.Status)
+		p.log.Printf("[ERROR] %s\n", err.Error())
+		return "", err
+	} else if cksum, err = common.GetChecksum([]byte(href)); err != nil {
+		p.log.Printf("[ERROR] Cannot compute checksum of URL %q: %s\n",
+			href,
+			err.Error())
+		return "", err
+	} else if suffix, err = getFileSuffix(resp); err != nil {
+		p.log.Printf("[ERROR] %s\n", err.Error())
+		return "", err
+	}
+
+	localPath = filepath.Join(
+		common.CacheDir,
+		fmt.Sprintf("%s.%s",
+			cksum,
+			suffix))
+
+	p.log.Printf("[TRACE] Save %q to %s\n",
+		href,
+		localPath)
+
+	if fh, err = os.Create(localPath); err != nil {
+		p.log.Printf("[ERROR] Cannot create local file %s: %s\n",
+			localPath, err.Error())
+		return "", err
+	}
+
+	defer fh.Close() // nolint: errcheck
+
+	if _, err = io.Copy(fh, resp.Body); err != nil {
+		p.log.Printf("[ERROR] Failed to save HTTP response for %q to %s: %s\n",
+			href,
+			localPath,
+			err.Error())
+		os.Remove(localPath) // nolint: errcheck
+		return "", err
+	}
+
+	return localPath, nil
+} // func (p *Prefetcher) fetchImage(href string) (string, error)
+
+var suffixPattern = regexp.MustCompile("(?i)^image/([a-z]+)$")
+
+func getFileSuffix(resp *http.Response) (string, error) {
+	var (
+		mime  string
+		match []string
+	)
+
+	mime = resp.Header["Content-Type"][0]
+
+	if match = suffixPattern.FindStringSubmatch(mime); match == nil {
+		return "", fmt.Errorf("Could not parse MIME type %q", mime)
+	}
+
+	return match[1], nil
+} // func getFileSuffix(resp *http.response) string
