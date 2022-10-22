f20b24b8569f1c1934bcef3fcc9e442dca3be390
Benjamin Walkenhorst <krylon@posteo.de>
2022-10-22 17:12:02 +0200

    Adjust path in shell script for cleaning up

--- clean.sh
+++ clean.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
+# Time-stamp: <2022-10-22 17:11:55 krylon>
 
-cd $GOPATH/src/ticker/
+cd $GOPATH/src/github.com/blicero/ticker/
 
 rm -vf bak.ticker ticker dbg.build.log && du -sh . && git fsck --full && git reflog expire --expire=now && git gc --aggressive --prune=now && du -sh .
 
